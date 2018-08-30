71fd1bc6dbada16dc3a2829909173b6e6dede0ca
mmajdanski <michael.majdanski@gmail.com>
2018-08-29 23:06:09 -0700

    Added logic for flipping cards as well as CSS transitions

--- src/containers/CardContainer.js
+++ src/containers/CardContainer.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 import Card from './../components/Card';
+import './Card.css'
+
+
 
 class CardContainer extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      flipped: false
+    }
+  }
+
+
+  cardFlip = (props) => {
+    //Adjust property of Flipped on the card to be true
+    props.flipped = true;
+    //Change State of Flipped Card, Needed in Order for CSS Transition to Work
+    this.setState({flipped: true})
+  }
+
+
+
   render() {
     return (
-      <Card />
+      <Card className={this.state.flipped ? 'flip backofCard' : 'fade'} onClick={() => this.cardFlip(this.props.card)}/>
     )
   }
 }
