f8c7dcc10d8166229fe406903b60483b66b4fb35
Warao Gil <wgil14@gmail.com>
2018-09-03 10:57:03 -0400

    Test cards flip.

--- src/containers/CardContainer.js
+++ src/containers/CardContainer.js
@@ -2,35 +2,24 @@ import React, { Component } from "react";
 
 import Card from "./../components/Card";
 import CardPropType from "./../propTypes/CardPropType";
-import "./Card.css";
 
 class CardContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      flipped: false
-    };
-  }
+  state = {
+    flipped: false
+  };
 
-  flip = props => {
-    //Adjust property of Flipped on the card to be true
-    props.flipped = !props.flipped;
-    //Change State of Flipped Card, Needed in Order for CSS Transition to Work
+  flip = () => {
     this.setState(prevState => {
       return { flipped: !prevState.flipped };
     });
   };
 
+  handleClick = () => {
+    this.flip();
+  };
+
   render() {
-    return (
-      <Card
-        className={this.state.flipped ? "card is-flipped" : "card fade"}
-        onClick={() => this.flip(this.props.card)}
-      >
-        <div className="card__face card__face--front" />
-        <div className="card__face card__face--back" />
-      </Card>
-    );
+    return <Card flipped={this.state.flipped} onClick={this.handleClick} />;
   }
 }
 
